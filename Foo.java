6a0a8350911f9e1cd6b9c307b47fc8c0e9453dff
jsinger1225 <jamie.singer@gmail.com>
2022-09-21 12:27:06 -0400

    Create Foo.java

---
+++ Foo.java
@@ -0,0 +1,12 @@
+package jbs;
+
+public class Foo {
+
+    public static void main( String... args ) {
+        System.out.println( "Foo main ..." );
+    }
+
+    public int foo() {
+        return 1;
+    }
+}
